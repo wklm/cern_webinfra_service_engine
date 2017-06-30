2024b490e44c24cb0800e9914b02ca87a50c528d
Wojciech Kulma <wojciech.kulma@cern.ch>
2017-06-30 16:36:50 +0200

    package fix

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='cern_webinfra_service_engine',
+    packages=['cern_webinfra_service_engine'],
+    version='0.1',
+    description='Library for connecting the microservices with CERN web infrastructure manager',
+    author='CERN / Wojciech Kulma',
+    author_email='wojciech.kulma@cern.ch',
+    url='https://github.com/wklm/cern_webinfra_service_engine.git',
+    download_url='https://github.com/wklm/cern_webinfra_service_engine.git',
+    keywords=['cern', 'webinfra', 'activemq', 'stomp',
+              'microservices',
+              'integration'],
+    classifiers=[],
+)
